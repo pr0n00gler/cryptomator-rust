2605ba8d6ddaa80b30b1f5de95a10279664f4517
Denis Kayshev <d.kayshev@rambler-co.ru>
2020-11-09 18:24:25 +0300

    fix imports

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,15 @@
 use failure::Fail;
-use std::io;
 
 #[derive(Debug, Fail)]
 pub enum MasterKeyError {
     #[fail(display = "Input/Output error")]
-    IOError(io::Error),
+    IOError(std::io::Error),
     #[fail(display = "Json serialize error")]
-    SerializationJsonError(serde_json::error::Error),
+    SerializationJsonError(serde_json::Error),
 }
 
-impl From<io::Error> for MasterKeyError {
-    fn from(err: io::Error) -> MasterKeyError {
+impl From<std::io::Error> for MasterKeyError {
+    fn from(err: std::io::Error) -> MasterKeyError {
         MasterKeyError::IOError(err)
     }
 }

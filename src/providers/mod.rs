3a30f8ea2764f0e90408903f4e79b922f1238c17
Mikhail Mozhaev <programmer10110@gmail.com>
2026-02-27 18:42:01 +0200

    add S3 provider module scaffold

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,5 +1,7 @@
 mod local_fs;
 mod mem_fs;
+mod s3_fs;
 
 pub use self::local_fs::LocalFs;
 pub use self::mem_fs::MemoryFs;
+pub use self::s3_fs::S3Fs;
---
+++ src/providers/s3_fs.rs
@@ -0,0 +1,3 @@
+/// S3-backed filesystem provider (implementation forthcoming).
+#[derive(Debug, Default)]
+pub struct S3Fs;
